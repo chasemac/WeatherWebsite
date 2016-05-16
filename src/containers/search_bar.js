3c962715355fae31a56c750f8001f57d1754c166
Chase McElroy <ichasem@gmail.com>
2016-05-16 11:38:27 -0700

    updated onInputChange function to allow typing in Search Bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+constructor(props) {
+  super(props);
+
+  this.state = { term: ''};
+
+  this.onInputChange = this.onInputChange.bind(this);
+}
+
+onInputChange(event) {
+  console.log(event.target.value);
+  this.setState({ term: event.target.value });
+
+}
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placehoder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}  />
         <span className="input-grou-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
-
       </form>
     );
   }
